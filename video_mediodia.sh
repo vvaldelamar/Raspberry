0828c2510731c7d41582d8caa7eefb332083b5d3
Conjunto Belen <vvaldelamar@gmail.com>
2023-10-10 08:35:59 -0600

    10/10/2023

--- video_mediodia.sh
+++ video_mediodia.sh
@@ -3,7 +3,7 @@
 cd /media/pi/DATOS/video_web
 
 #PUERTA_PEATONAL
-find /media/pi/DATOS/DATOS-b/PUERTA_PEATONAL/`date "+%Y-%m-%2d"`/pic_001 -name "*.jpg" -type f -printf "%T@\t%Tc %6k KiB %p\n" | sort -n |awk '{print "file "$11}' > image_dahua_01.txt
+find /media/pi/DATOS/DATOS-b/9F05661PAG445ED/`date "+%Y-%m-%2d"`/001/jpg/ -name "*.jpg" -type f -printf "%T@\t%Tc %6k KiB %p\n" | sort -n |awk '{print "file "$11}' > image_dahua_01.txt
 ffmpeg -y -r 1 -f concat -safe 0 -i image_dahua_01.txt -vcodec h264 -vf "fps=20,scale=-1:720,format=yuv420p,setpts=0.25*PTS" output_dahua_01_`date "+%Y%m%2d"`.mp4
 ffmpeg -y -i output_dahua_01_`date "+%Y%m%2d"`.mp4 -hls_time 2 -hls_key_info_file /media/pi/DATOS/enc.keyinfo -hls_playlist_type vod -hls_segment_filename "output_dahua_01_`date "+%Y%m%2d"`_%d.ts" output_dahua_01_`date "+%Y%m%2d"`.m3u8
 /media/pi/DATOS/spritevideo -i output_dahua_01_`date "+%Y%m%2d"`.mp4 -o /media/pi/DATOS/video_web -p output_dahua_01_`date "+%Y%m%2d"`.jpg
@@ -12,8 +12,7 @@ rm -rf output_dahua_01_`date "+%Y%m%2d"`.mp4
 rm -rf image_dahua_01.txt
 
 #PUERTA DE ENTRADA
-# /media/pi/DATOS/DATOS-b/PUERTA_ENTRADA
-find /media/pi/DATOS/DATOS-b/PUERTA_ENTRADA/`date "+%Y-%m-%2d"`/001/jpg/ -name "*.jpg" -type f -printf "%T@\t%Tc %6k KiB %p\n" | sort -n |awk '{print "file "$11}' >image_dahua_02.txt
+find /media/pi/DATOS/DATOS-b/9F05661PAGB373F/`date "+%Y-%m-%2d"`/001/jpg/ -name "*.jpg" -type f -printf "%T@\t%Tc %6k KiB %p\n" | sort -n |awk '{print "file "$11}' >image_dahua_02.txt
 ffmpeg -y -r 1 -f concat -safe 0 -i image_dahua_02.txt -vcodec h264  -vf "fps=20,scale=-1:720,format=yuv420p,setpts=0.25*PTS" output_dahua_02_`date "+%Y%m%2d"`.mp4
 ffmpeg -y -i output_dahua_02_`date "+%Y%m%2d"`.mp4 -hls_time 2 -hls_key_info_file /media/pi/DATOS/enc.keyinfo -hls_playlist_type vod -hls_segment_filename "output_dahua_02_`date "+%Y%m%2d"`_%d.ts" output_dahua_02_`date "+%Y%m%2d"`.m3u8
 /media/pi/DATOS/spritevideo -i output_dahua_02_`date "+%Y%m%2d"`.mp4 -o /media/pi/DATOS/video_web -p output_dahua_02_`date "+%Y%m%2d"`.jpg
